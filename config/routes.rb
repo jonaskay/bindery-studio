0ae79a83ae7b14b704999ca410a1361f202c5932
jonaskay <jonaskay@iki.fi>
2020-06-25 22:53:53 +0300

    Organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+  devise_for :users
 
   get 'welcome/index'
 end
