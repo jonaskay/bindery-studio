3bd86e21986e14d8cd496609f341bfb818bc1c25
jonaskay <jonaskay@iki.fi>
2020-07-16 14:45:20 +0300

    Handle chromedriver downloads

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,3 +1,10 @@
 require 'webmock/rspec'
 
-WebMock.disable_net_connect!(allow_localhost: true, allow: ['selenium:4444', 'test:3001'])
+WebMock.disable_net_connect!(
+  allow_localhost: true,
+  allow: [
+    'http://selenium:4444',
+    'http://test:3001',
+    'https://chromedriver.storage.googleapis.com'
+  ]
+)
