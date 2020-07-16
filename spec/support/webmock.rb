2e49b097762d0f06e543df551f0ae90d5af972f8
jonaskay <jonaskay@iki.fi>
2020-07-16 14:39:24 +0300

    Allow localhost requests during tests

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,3 +1,3 @@
 require 'webmock/rspec'
 
-WebMock.disable_net_connect!(allow: ['selenium:4444', 'test:3001'])
+WebMock.disable_net_connect!(allow_localhost: true, allow: ['selenium:4444', 'test:3001'])
