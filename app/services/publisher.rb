ea1b5885b9228e4ece1f09f2f07c3be0f0b7ae5a
jonaskay <jonaskay@iki.fi>
2020-08-08 14:21:38 +0300

    Remove unpublish instance template

--- app/services/publisher.rb
+++ app/services/publisher.rb
@@ -4,23 +4,18 @@ require 'google/apis/compute_v1'
 class Publisher
   class Error < StandardError; end
 
-  PUBLISH = :publish
-  UNPUBLISH = :unpublish
-
   attr_reader :project, :service, :google_cloud_project, :compute_engine_template, :compute_engine_zone
 
   def self.publish(project)
-    template = ENV["COMPUTE_ENGINE_PUBLISH_TEMPLATE"]
-
-    self.new(project, PUBLISH).insert_instance
+    self.new(project).insert_instance
   end
 
-  def initialize(project, compute_engine_template)
+  def initialize(project)
     @project = project
     @service = Google::Apis::ComputeV1::ComputeService.new
 
     @google_cloud_project = ENV.fetch("GOOGLE_CLOUD_PROJECT")
-    @compute_engine_template = compute_engine_template
+    @compute_engine_template = ENV.fetch("COMPUTE_ENGINE_PUBLISH_TEMPLATE")
     @compute_engine_zone = ENV.fetch("COMPUTE_ENGINE_ZONE")
 
     authenticate_service
@@ -29,25 +24,17 @@ class Publisher
   def insert_instance
     instance_name = "#{compute_engine_template}-#{project.id}"
     instance = Google::Apis::ComputeV1::Instance.new(name: instance_name)
+    instance_template = "global/instanceTemplates/#{compute_engine_template}"
 
     service.insert_instance(
       google_cloud_project,
       compute_engine_zone,
       instance,
-      source_instance_template: source_instance_template
+      source_instance_template: instance_template
     )
   end
 
   private
-
-  def source_instance_template
-    instance_templates = {
-      publish: ENV["COMPUTE_ENGINE_PUBLISH_TEMPLATE"],
-      unpublish: ENV["COMPUTE_ENGINE_UNPUBLISH_TEMPLATE"]
-    }
-
-    "global/instanceTemplates/#{instance_templates[compute_engine_template]}"
-  end
 
   def authenticate_service
     scopes = ["https://www.googleapis.com/auth/compute"]
