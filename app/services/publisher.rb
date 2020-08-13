07b96842435a342db3f08de5ff31d4b6155caadd
jonaskay <jonaskay@iki.fi>
2020-08-13 21:07:30 +0300

    Register deployments

--- app/services/publisher.rb
+++ app/services/publisher.rb
@@ -1,36 +1,39 @@
-require 'googleauth'
-require 'google/apis/compute_v1'
+require "googleauth"
+require "google/apis/compute_v1"
 
 class Publisher
   class Error < StandardError; end
 
-  attr_reader :project, :service, :google_cloud_project, :compute_engine_template, :compute_engine_zone
+  attr_reader :project, :service, :cloud_project, :instance_template, :compute_zone
 
   def self.publish(project)
-    self.new(project).insert_instance
+    publisher = self.new(project)
+    instance = "#{publisher.instance_template}-#{project.id}"
+
+    self.new(project).insert_instance(instance)
+    project.deployments.create!(instance: instance)
   end
 
   def initialize(project)
     @project = project
     @service = Google::Apis::ComputeV1::ComputeService.new
 
-    @google_cloud_project = ENV.fetch("GOOGLE_CLOUD_PROJECT")
-    @compute_engine_template = ENV.fetch("COMPUTE_ENGINE_COMPOSITOR_TEMPLATE")
-    @compute_engine_zone = ENV.fetch("COMPUTE_ENGINE_ZONE")
+    @cloud_project = ENV.fetch("GOOGLE_CLOUD_PROJECT")
+    @instance_template = ENV.fetch("COMPUTE_ENGINE_COMPOSITOR_TEMPLATE")
+    @compute_zone = ENV.fetch("COMPUTE_ENGINE_ZONE")
 
     authenticate_service
   end
 
-  def insert_instance
-    instance_name = "#{compute_engine_template}-#{project.id}"
-    instance = Google::Apis::ComputeV1::Instance.new(name: instance_name)
-    instance_template = "global/instanceTemplates/#{compute_engine_template}"
+  def insert_instance(instance)
+    compute_instance = Google::Apis::ComputeV1::Instance.new(name: instance)
+    template_url = "global/instanceTemplates/#{instance_template}"
 
     service.insert_instance(
-      google_cloud_project,
-      compute_engine_zone,
-      instance,
-      source_instance_template: instance_template
+      cloud_project,
+      compute_zone,
+      compute_instance,
+      source_instance_template: template_url
     )
   end
 
--- spec/services/publisher_spec.rb
+++ spec/services/publisher_spec.rb
@@ -1,24 +1,47 @@
-require 'rails_helper'
-require 'support/helpers/googleapis_helper'
+require "rails_helper"
+require "support/helpers/googleapis_helper"
+
+RSpec.shared_examples "successful insert instance request" do
+  it "sends an insert instance request" do
+    subject
+
+    expect(insert_instance_stub).to have_been_requested
+  end
+end
 
 RSpec.describe Publisher, type: :model do
   include GoogleapisHelper
 
+  let(:project) { create(:project) }
+  let!(:insert_instance_stub) {
+    stub(:compute, :insert_instance, params: {
+      "project" => "my-project",
+      "zone" => "my-zone",
+      "template" => "my-compositor-template"
+    }).with_json('{ "id": "42" }')
+  }
+
+  before { handle_oauth_request }
+
   describe ".publish" do
-    before do
-      handle_oauth_request
-
-      stub(:compute, :insert_instance, params: {
-        "project" => "my-project",
-        "zone" => "my-zone",
-        "template" => "my-compositor-template"
-      }).with_json('{ "id": "42" }')
-    end
-
-    let(:project) { create(:project, id: 1337) }
-
     subject { described_class.publish(project) }
 
-    it { is_expected.to be_a(Google::Apis::ComputeV1::Operation) }
+    include_examples "successful insert instance request"
+
+    it "creates a new deployment" do
+      expect { subject }.to change { project.deployments.count }.by(1)
+    end
+  end
+
+  describe "#insert_instance" do
+    let(:publisher) { Publisher.new(project) }
+
+    subject { publisher.insert_instance("foo") }
+
+    include_examples "successful insert instance request"
+
+    it "returns an operation object" do
+      expect(subject).to be_a(Google::Apis::ComputeV1::Operation)
+    end
   end
 end
