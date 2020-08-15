fc7bf5866b647886f97dc57f132aa814b072394b
jonaskay <jonaskay@iki.fi>
2020-08-15 19:11:51 +0300

    Restore cron job config

--- app/services/deployment_monitor.rb
+++ app/services/deployment_monitor.rb
@@ -1,7 +1,7 @@
 class DeploymentMonitor
   def self.check_healths
     Deployment.pending.find_each do |deployment|
-      if deployment.created_at < 5.minutes.ago
+      if deployment.created_at < 1.hour.ago
         deployment.handle_timeout
       end
     end
