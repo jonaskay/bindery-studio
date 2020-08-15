c257b2c0ed1f256059cf3003c813b82c65d1f6ed
jonaskay <jonaskay@iki.fi>
2020-08-15 19:52:36 +0300

    Log headers

--- app/controllers/tasks/base_controller.rb
+++ app/controllers/tasks/base_controller.rb
@@ -12,6 +12,7 @@ class Tasks::BaseController < ActionController::API
   def validate_client!
     ip = request.remote_ip
     logger.debug("Remote IP: #{ip}")
+    logger.debug("Headers: #{request.headers}")
     head :forbidden if ip != "10.0.0.1"
   end
 end
